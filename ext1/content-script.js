82dbb462ff78ae541bc843f71c04c3aff48cb491
Artem Nefedov <artemnefedov777@gmail.com>
2023-03-11 11:21:57 +0500

    Add files via upload

---
+++ ext1/content-script.js
@@ -0,0 +1,20 @@
+// function deleteBlock(){
+//     console.log(123)
+//     !!document
+//     .getElementsByClassName("currency-rates__container-3P")
+//     &&
+//     document
+//     .getElementsByClassName("currency-rates__container-3P")[0].remove();
+
+// }
+
+// deleteBlock()
+
+function ColorBlock(){
+    console.log(123)
+    !!document
+    .getElementsByClassName("ContributionCalendar-day")
+    &&
+    document
+    .getElementsByClassName("ContributionCalendar-day")[0].style.backgroundColor == "#45a12e";
+}
